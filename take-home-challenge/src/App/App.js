3ddbb6103b3ac352528e0b984cf3a2e17c264702
Alexys Jordan <alexyskjordan@gmail.com>
2023-06-06 14:08:24 -0600

    add router

---
+++ take-home-challenge/src/App/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  const [news, setNews] = useState([])
+  const [article, setArticle] = useState({})
+
+  
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
